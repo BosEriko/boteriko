0544568a6fd7625f574479d5de265762d7d0927a
BosEriko <bos@eriko.ph>
2025-06-19 10:14:33 +0800

    Feature: Update isStreamingUtility to check Twitch Status instead

--- global/utilities/isStreaming.js
+++ global/utilities/isStreaming.js
@@ -1,8 +1,20 @@
-function isStreamingUtility() {
-  const start = parseInt(process.env.STREAM_START, 10);
-  const end = parseInt(process.env.STREAM_END, 10);
-  const currentHour = new Date().getHours();
-  return currentHour >= start && currentHour < end;
+const axios = require('axios');
+
+async function isStreamingUtility(channelName, clientId, accessToken) {
+  try {
+      const url = `https://api.twitch.tv/helix/streams?user_login=${channelName}`;
+      const response = await axios.get(url, {
+          headers: {
+              'Client-ID': clientId,
+              'Authorization': `Bearer ${accessToken}`
+          }
+      });
+
+      return response.data.data.length > 0;
+  } catch (error) {
+      console.error("Error fetching streamer status:", error);
+      return false;
+  }
 }
 
 module.exports = isStreamingUtility;
