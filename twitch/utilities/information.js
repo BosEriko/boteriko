45868efe5a295ed277d45e7600c31fea876ef958
BosEriko <bos@eriko.ph>
2025-06-21 13:17:03 +0800

    Feature: Use Constants for Information Rotator

--- twitch/utilities/information.js
+++ twitch/utilities/information.js
@@ -1,13 +1,6 @@
-let messageIndex = 0;
+const informationConstant = require('@global/constants/information');
 
-const messages = [
-  "Join me on Discord (http://discord.boseriko.com)! Maybe we can watch Anime there after my stream?",
-  "Follow me on TikTok (http://tiktok.boseriko.com) and YouTube (http://youtube.boseriko.com)!",
-  "If you're interested here's my Anime list (http://anime.boseriko.com) and my Manga list (http://anime.boseriko.com)!",
-  'Type the words falling down on the screen and have fun! Do "!top" to see the top typers of the current stream!',
-  'Need a deez nuts joke? Type "!dn" followed by a topic, like "!dn tetris" or "!dn anime"',
-  'Want to know your points? Type "!points" to see how many points you have!'
-];
+let informationIndex = 0;
 
 function shuffle(array) {
   for (let i = array.length - 1; i > 0; i--) {
@@ -16,13 +9,13 @@ function shuffle(array) {
   }
 }
 
-shuffle(messages);
+shuffle(informationConstant);
 
 function handleInformationUtility(client, username) {
-  const message = messages[messageIndex];
+  const message = informationConstant[informationIndex];
   client.say(`#${username}`, `📢 ${message}`);
 
-  messageIndex = (messageIndex + 1) % messages.length;
+  informationIndex = (informationIndex + 1) % informationConstant.length;
 }
 
 module.exports = handleInformationUtility;
