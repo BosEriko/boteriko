a6991b794363a0da65922e09f363187cac9584ee
BosEriko <bos@eriko.ph>
2025-06-21 14:20:01 +0800

    Feature: Create env utility

---
+++ global/utilities/env.js
@@ -0,0 +1,41 @@
+const env = {
+    app: {
+        clientUrl: process.env.APP_CLIENT_URL
+    },
+    twitch: {
+        channel: {
+            username: process.env.TWITCH_CHANNEL_USERNAME,
+            id: process.env.TWITCH_CHANNEL_ID
+        },
+        bot: {
+            username: process.env.TWITCH_BOT_USERNAME,
+            accessToken: process.env.TWITCH_BOT_ACCESS_TOKEN,
+            refreshToken: process.env.TWITCH_BOT_REFRESH_TOKEN,
+            clientId: process.env.TWITCH_BOT_CLIENT_ID
+        },
+        app: {
+            clientId: process.env.TWITCH_APP_CLIENT_ID,
+            clientSecret: process.env.TWITCH_APP_CLIENT_SECRET,
+            redirectUrl: process.env.TWITCH_APP_REDIRECT_URL
+        }
+    },
+    discord: {
+        serverId: process.env.DISCORD_SERVER_ID,
+        botToken: process.env.DISCORD_BOT_TOKEN,
+        app: {
+            clientId: process.env.DISCORD_APP_CLIENT_ID,
+            clientSecret: process.env.DISCORD_APP_CLIENT_SECRET,
+            redirectUrl: process.env.DISCORD_APP_REDIRECT_URL
+        }
+    },
+    openrouter: {
+        apiKey: process.env.OPENROUTER_API_KEY
+    },
+    firebase: {
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+    }
+};
+
+module.exports = env;
