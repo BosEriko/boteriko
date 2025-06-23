50d3c859bfa09851398051a172eb2b72ef5f0327
BosEriko <bos@eriko.ph>
2025-06-23 13:56:28 +0800

    Feature: Use global state

---
+++ global/utilities/state.js
@@ -0,0 +1,4 @@
+module.exports = {
+  isStreaming: false,
+  lastMessageTimestamp: Date.now()
+};
--- twitch/utilities/cron.js
+++ twitch/utilities/cron.js
@@ -1,62 +1,61 @@
 const cron = require('node-cron');
 const env = require('@global/utilities/env');
+const state = require('@global/utilities/state');
 
 // Utilities
 const handleIsStreamingUtility = require("@global/utilities/isStreaming");
 const handleInformationUtility = require('@twitch/utilities/information');
 const handleFollowUtility = require('@twitch/utilities/follow');
 const handleSetupUtility = require('@twitch/utilities/setup');
-
-let isStreaming = false;
-let lastMessageTimestamp = Date.now();
+const handleTopicCommand = require('@global/commands/topic');
 
 function handleCronUtility(client) {
-    // ------------------------------------------- Functions -------------------------------------------
-    
-    // isStreaming Utility
-    async function checkStreamAvailability() {
-        try {
-            isStreaming = await handleIsStreamingUtility();
-        } catch (error) {
-            console.error("Error checking stream availability:", error.message);
-        }
+  // ------------------------------------------- Functions -------------------------------------------
+
+  // isStreaming Utility
+  async function checkStreamAvailability() {
+    try {
+      state.isStreaming = await handleIsStreamingUtility();
+    } catch (error) {
+      console.error("Error checking stream availability:", error.message);
     }
-    
-    // Conversation Utility
-    async function runConversationUtility() {
-        const now = Date.now();
-        const fiveMinutes = 5 * 60 * 1000;
-        
-        if (now - lastMessageTimestamp >= fiveMinutes) {
-            client.say(`#${env.twitch.channel.username}`, `💭 ${await handleTopicCommand()}`);
-            lastMessageTimestamp = now;
-        }
+  }
+
+  // Conversation Utility
+  async function runConversationUtility() {
+    const now = Date.now();
+    const fiveMinutes = 5 * 60 * 1000;
+
+    if (now - state.lastMessageTimestamp >= fiveMinutes) {
+      client.say(`#${env.twitch.channel.username}`, `💭 ${await handleTopicCommand()}`);
+      state.lastMessageTimestamp = now;
     }
-    
-    // follow Utility
-    function checkNewFollowers() {
-        const username = env.twitch.channel.username;
-        handleFollowUtility(newFollower => client.say(`#${username}`, `${newFollower} just followed!`));
-    }
-    
-    // ------------------------------------------- Cron Jobs -------------------------------------------
-    
-    // Every 1 minute
-    cron.schedule('* * * * *', () => {
-        handleSetupUtility(client);
-        if (isStreaming) checkNewFollowers();
-        if (isStreaming) runConversationUtility();
-    });
-    
-    // Every 5 minutes
-    cron.schedule('*/5 * * * *', () => {
-        checkStreamAvailability();
-    });
-    
-    // Every 10 minutes
-    cron.schedule('*/10 * * * *', () => {
-        if (isStreaming) handleInformationUtility(client);
-    });
+  }
+
+  // follow Utility
+  function checkNewFollowers() {
+    const username = env.twitch.channel.username;
+    handleFollowUtility(newFollower => client.say(`#${username}`, `${newFollower} just followed!`));
+  }
+
+  // ------------------------------------------- Cron Jobs -------------------------------------------
+
+  // Every 1 minute
+  cron.schedule('* * * * *', () => {
+    handleSetupUtility(client);
+    if (state.isStreaming) checkNewFollowers();
+    if (state.isStreaming) runConversationUtility();
+  });
+
+  // Every 5 minutes
+  cron.schedule('*/5 * * * *', () => {
+    checkStreamAvailability();
+  });
+
+  // Every 10 minutes
+  cron.schedule('*/10 * * * *', () => {
+    if (state.isStreaming) handleInformationUtility(client);
+  });
 }
 
 module.exports = handleCronUtility;
