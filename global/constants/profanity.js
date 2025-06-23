fdee96a838e151550ca71e857409782d9e80e0bf
BosEriko <bos@eriko.ph>
2025-06-23 20:14:20 +0800

    Feature: Create Profanity Constant

---
+++ global/constants/profanity.js
@@ -0,0 +1,19 @@
+const profanityWords = [
+  // Tagalog
+  "burat", "titi", "puke", "puki", "kantot", "puta",
+  "gago", "gaga", "tangina", "leche", "letse", "bayag", "pekpek",
+  "kupal", "bobo", "tanga", "hinayupak", "animal", "bwisit",
+  "ogag", "demoniyo", "lintik",
+
+  // Bisaya
+  "yawa", "buang", "pisti", "pista", "unggoy", "itlog", "otin", "oten",
+  "bilat", "giatay", "yawat", "ilaga ka", "kagwang", "tabian",
+
+  // English
+  "fuck", "shit", "bitch", "asshole", "cunt", "dick", "pussy", "cock",
+  "bastard", "whore", "slut", "dildo",
+  "faggot", "jerk", "damn", "crap", "douche", "twat", "prick",
+  "blowjob", "handjob", "masturbate", "orgasm"
+];
+
+module.exports = profanityWords;
