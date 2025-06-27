fff1b56892c319df0c92c4606af0a00525c5bd9d
BosEriko <bos@eriko.ph>
2025-06-27 11:19:32 +0800

    Feature: Update Information Constant to remove Points

--- global/constants/information.js
+++ global/constants/information.js
@@ -1,11 +1,10 @@
 const informationConstant = [
-  "Join me on Discord (http://discord.boseriko.com)! Maybe we can watch Anime there after my stream?",
+  "Join me on Discord (http://discord.boseriko.com)!",
   "Follow me on TikTok (http://tiktok.boseriko.com) and YouTube (http://youtube.boseriko.com)!",
-  "If you're interested here's my Anime list (http://anime.boseriko.com) and my Manga list (http://anime.boseriko.com)!",
+  "If you're interested here's my Anime list (http://anime.boseriko.com) and my Manga list (http://manga.boseriko.com)!",
   'Type the words falling down on the screen and have fun! Do "!top" to see the top typers of the current stream!',
   'Need a deez nuts joke? Type "!dn" followed by a topic, like "!dn tetris" or "!dn anime"!',
   'Need a topic? Type "!topic" to get a random topic for discussion!',
-  'Want to know your points? Type "!points" to see how many points you have!'
 ];
 
 module.exports = informationConstant;
