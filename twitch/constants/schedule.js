01b6c35c8873f7d105cd81ac0878ef7af66ea2b1
BosEriko <bos@eriko.ph>
2025-07-14 23:15:59 +0800

    Feature: Switch Tetris and Tilted schedule

--- twitch/constants/schedule.js
+++ twitch/constants/schedule.js
@@ -1,8 +1,8 @@
 const scheduleConstant = {
   Monday: { title: "Minecraft Monday 🌍⛏️", category: "Minecraft" },
-  Tuesday: { title: "Tilted Tuesday 🔫💥", category: "Fortnite" },
+  Tuesday: { title: "Tetris Tuesday 🎮🟨", category: "TETR.IO" },
   Wednesday: { title: "Waifu Wednesday ⚔️🍀", category: "Wuthering Waves" },
-  Thursday: { title: "Tetris Thursday 🎮🟨", category: "TETR.IO" },
+  Thursday: { title: "Tilted Thursday 🔫💥", category: "Fortnite" },
   Friday: { title: "Fun Fun Friday 🎮🆕", category: "Games + Demos" },
   Saturday: { title: "Side Quest Saturday 💬🎯", category: "Just Chatting" },
   Sunday: { title: "Slow Down Sunday 💻🎮", category: "Just Chatting" },
