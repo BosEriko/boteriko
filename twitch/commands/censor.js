71f503d42d867a1d994f8b92e83b989c54d15c33
BosEriko <bos@eriko.ph>
2025-07-20 12:48:45 +0800

    Feature: Update Censor Message on Chat

--- twitch/commands/censor.js
+++ twitch/commands/censor.js
@@ -2,7 +2,7 @@ const { broadcastToClient } = require('@global/utilities/websocket');
 
 function handleCensorCommand(client, channel) {
   const timestamp = Date.now();
-  client.say(channel, "Toggling censorship settings in the overlay...");
+  client.say(channel, "Censorship Toggled");
   broadcastToClient({ type: 'CENSOR', timestamp });
 }
 
