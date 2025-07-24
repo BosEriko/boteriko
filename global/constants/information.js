02322150b4fb8a64f6e17867bbf21be8d12a08ee
BosEriko <bos@eriko.ph>
2025-07-24 15:08:19 +0800

    Feature: Remove commands related announcements in favor of !commands

--- global/constants/information.js
+++ global/constants/information.js
@@ -3,14 +3,9 @@ const informationConstant = [
   `Follow me on TikTok (http://tiktok.boseriko.com) and YouTube (http://youtube.boseriko.com)!`,
   `If you're interested here's my Anime list (http://anime.boseriko.com) and my Manga list (http://manga.boseriko.com)!`,
   `Join me on Discord (http://discord.boseriko.com)!`,
-  `Need a deez nuts joke? Type "!dn" followed by a topic, like "!dn tetris" or "!dn anime"!`,
-  `Need a fact? Type "!fact" to get a random fact!`,
-  `Need a topic? Type "!topic" to get a random topic for discussion!`,
   `Share your links on the chat! All of them will be saved to Discord (http://discord.boseriko.com)!`,
   `Type the words falling down on the screen and have fun! Each correct word earns you 💰 2 Bos Coins! The top typer of today's stream will be featured in tomorrow's stream title! Use "!top" to see who's leading!`,
   `Want a full list of commands? Type "!commands" in Discord (http://discord.boseriko.com) to check them out!`,
-  `Want to know when I stream? Type "!schedule" to see my stream schedule!`,
-  `Want to know your wallet balance? Type "!wallet" to see the amount of Bos Coins you have!`,
 ];
 
 module.exports = informationConstant;
