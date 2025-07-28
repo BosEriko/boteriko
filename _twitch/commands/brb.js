e5c32ef917b28a19a655b4980dd3f8f86e7a2131
BosEriko <bos@eriko.ph>
2025-07-28 15:20:57 +0800

    Feature: Add Music to BRB Scene

--- _twitch/commands/brb.js
+++ _twitch/commands/brb.js
@@ -10,6 +10,7 @@ function handleBrbCommand(client, channel, prompt) {
   if (prompt) {
     client.say(channel, `Be right back! ${toTitleCase(prompt) || ""}`);
     broadcastToClient({ type: 'TICKER', message: prompt, isVisible: true });
+    broadcastToClient({ type: 'MUSIC', id: 'THE_GRAND_AFFAIR_COUPE', isVisible: true });
   } else {
     client.say(channel, "Please provide a message: !brb <message>");
   }
