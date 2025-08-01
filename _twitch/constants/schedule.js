a45f79948d4436a238585a08cf061459e079d848
BosEriko <bos@eriko.ph>
2025-08-01 16:12:53 +0800

    Feature: Update schedule to include workshop and footage

--- _twitch/constants/schedule.js
+++ _twitch/constants/schedule.js
@@ -1,9 +1,9 @@
 const scheduleConstant = {
   Monday: { title: "Minecraft Monday 🌍⛏️", category: "Minecraft" },
   Tuesday: { title: "Tetris Tuesday 🎮🟨", category: "TETR.IO" },
-  Wednesday: { title: "Waifu Wednesday ⚔️🍀", category: "Wuthering Waves" },
+  Wednesday: { title: "Workshop Wednesday 💻🛠️", category: "Software and Game Development" },
   Thursday: { title: "Tilted Thursday 🔫💥", category: "Fortnite" },
-  Friday: { title: "Foreign Friday 🌍🦜", category: "Duolingo" },
+  Friday: { title: "Footage Friday 🎬✂️", category: "Makers & Crafting" },
   Saturday: { title: "Side Quest Saturday 💬🎯", category: "Just Chatting" },
   Sunday: { title: "Slow Down Sunday 💻🎮", category: "Just Chatting" },
 };
