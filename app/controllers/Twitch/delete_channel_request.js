207f5041982ab075caa3b01378f0f379706822bb
BosEriko <bos@eriko.ph>
2025-08-07 15:39:40 +0800

    Hotfix: Move status to params on delete_channel_request

--- app/controllers/Twitch/delete_channel_request.js
+++ app/controllers/Twitch/delete_channel_request.js
@@ -4,21 +4,22 @@ const handleErrorUtility = require('@global/utilities/error');
 
 const delete_channel_request = async (reward, id) => {
   try {
-    await axios.patch(`https://api.twitch.tv/helix/channel_points/custom_rewards/redemptions`, null, {
-      params: {
-        broadcaster_id: env.twitch.channel.id,
-        reward_id: reward.id,
-        id: id,
-      },
-      headers: {
-        'Client-ID': env.twitch.channel.clientId,
-        'Authorization': `Bearer ${env.twitch.channel.accessToken}`,
-        'Content-Type': 'application/json',
-      },
-      data: {
-        status: "CANCELED"
+    await axios.patch(
+      `https://api.twitch.tv/helix/channel_points/custom_rewards/redemptions`,
+      null,
+      {
+        params: {
+          broadcaster_id: env.twitch.channel.id,
+          reward_id: reward.id,
+          id: id,
+          status: "CANCELED",
+        },
+        headers: {
+          'Client-ID': env.twitch.channel.clientId,
+          'Authorization': `Bearer ${env.twitch.channel.accessToken}`,
+        }
       }
-    });
+    );
 
     return `💸 Refunded ${reward.title} redemption (${id})`;
   } catch (err) {
