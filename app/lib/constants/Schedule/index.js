7d8bb1a8fe72af8abb35d686e9185385f0922eb6
BosEriko <bos@eriko.ph>
2025-08-16 00:34:01 +0800

    Feature: Update Schedule and Remove Wuthering Waves

--- app/lib/constants/Schedule/index.js
+++ app/lib/constants/Schedule/index.js
@@ -1,9 +1,9 @@
 const Schedule = {
   Monday: { title: "Minecraft Monday 🌍⛏️", category: "Minecraft" },
   Tuesday: { title: "Tetris Tuesday 🎮🟨", category: "TETR.IO" },
-  Wednesday: { title: "Waifu Wednesday ⚔️🍀", category: "Wuthering Waves" },
+  Wednesday: { title: "Wreck-It Wednesday 💥👊", category: "Tekken 8" },
   Thursday: { title: "Tilted Thursday 🔫💥", category: "Fortnite" },
-  Friday: { title: "Fighting Friday 🥊🔥", category: "Tekken 8" },
+  Friday: { title: "Free-Games Friday 🎮✨", category: "Games + Demos" },
   Saturday: { title: "Side Quest Saturday 💬🎯", category: "Just Chatting" },
   Sunday: { title: "Slow Down Sunday 💻🎮", category: "Just Chatting" },
 };
