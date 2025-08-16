3b241d63406f6880041acc39e5f7e92b4539ece7
BosEriko <bos@eriko.ph>
2025-08-16 18:45:10 +0800

    Hotfix: Remove marking of duplicates as completed

--- app/controllers/Music/polling/index.js
+++ app/controllers/Music/polling/index.js
@@ -24,7 +24,7 @@ const updateQueue = (currentId) => {
         playingAssigned = true;
         return { ...item, status: "PLAYING" };
       } else {
-        return { ...item, status: "COMPLETED" };
+        return { ...item, status: "QUEUED" };
       }
     }
 
