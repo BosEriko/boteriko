bbcaa37cb467f832a47af2d294073a938df388c1
BosEriko <bos@eriko.ph>
2025-08-19 19:16:31 +0800

    Feature: Make isRegistered have a default false and remove default for Age

--- db/schema.js
+++ db/schema.js
@@ -2,11 +2,11 @@ module.exports = {
   users: {
     type: 'firestore',
     columns: {
-      age: { type: 'number', default: 0 },
+      age: { type: 'number' },
       discordId: { type: 'string' },
       displayName: { type: 'string', required: true },
       email: { type: 'string' },
-      isRegistered: { type: 'boolean' },
+      isRegistered: { type: 'boolean', default: false },
       profileImage: { type: 'string' },
     },
   },
