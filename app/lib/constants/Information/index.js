9cc0acf71cfdb029babd835f55d5a885b4ccbdf8
BosEriko <bos@eriko.ph>
2025-08-24 23:46:45 +0800

    Feature: Add Information Rotator for !profile command

--- app/lib/constants/Information/index.js
+++ app/lib/constants/Information/index.js
@@ -1,4 +1,5 @@
 const Information = [
+  `Check out your BosEriko+ profile with "!profile"! See your attendance, Bos Coins, and linked accounts. You can also tag someone like "!profile @username" to view their profile. Not registered yet? I'll send you your personalized invite link!`,
   `Music is now on! Use "!queue" to get the URL of the current song queue, "!song" to see the currently playing song, and add songs by redeeming Bos Points on Channel Rewards. Spotify links are supported!`,
   `Share your links on the chat! All of them will be saved to Discord (http://discord.boseriko.com)!`,
   `Type the words falling down on the screen and have fun! Each correct word earns you 💰 2 Bos Coins! The top typer of today's stream will be featured in tomorrow's stream title! Use "!top" to see who's leading!`,
