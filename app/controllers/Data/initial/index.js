5fd9942acdc18753dfe85bb9db7548f0817265c4
BosEriko <bos@eriko.ph>
2025-08-25 19:09:26 +0800

    Feature: Make Initial Data Endpoint

--- app/controllers/Data/initial/index.js
+++ app/controllers/Data/initial/index.js
@@ -1,8 +1,52 @@
 const express = require('express');
+const verify_firebase_token = require("../../concerns/verify_firebase_token");
+const cacheUtility = require('@global/utilities/cache');
+
 const initial = express.Router();
 
+const CACHE_DURATION = 30 * 60 * 1000;
+const initialDataCache = cacheUtility(CACHE_DURATION);
+
 initial.get('/', async (req, res) => {
-  console.log("Initial Data");
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({ success: false, message: "Unauthorized" });
+    }
+
+    const token = await verify_firebase_token(authHeader.split(" ")[1]);
+    const uid = token.uid;
+
+    const cached = initialDataCache.get(uid, 'initial-data');
+    if (cached) return res.json(cached);
+
+    const user = await Model.User.find(uid);
+
+    if (!user) {
+      return res.status(404).json({ success: false, message: "User doesn't exist" });
+    }
+
+    if (!user.attributes?.isRegistered) {
+      return res.status(400).json({ success: false, message: "User is not registered" });
+    }
+
+    const data = {
+      success: true,
+      cacheExpiresIn: CACHE_DURATION,
+      user,
+    }
+
+    const connection = await Model.Connection.find(uid);
+    if (connection) data.connection = connection;
+
+    initialDataCache.set(uid, data, 'initial-data');
+
+    res.json(data);
+  } catch (err) {
+    await Utility.error_logger("Initial Data fetching error:", err?.response?.data || err.message);
+    res.status(500).json({ success: false, message: "Fetching of Initial Data failed." });
+  }
 });
 
 module.exports = initial;
