480815a40d1ec0dfc530f81c0e41c751ca2ebab2
BosEriko <bos@eriko.ph>
2025-09-02 13:11:34 +0800

    Feature: Allow Search with no Token

--- app/controllers/Data/search/index.js
+++ app/controllers/Data/search/index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const verify_firebase_token = require("../../concerns/verify_firebase_token");
 const search_games = require("./game");
 const search_anime = require("./anime");
 const search_manga = require("./manga");
@@ -36,23 +35,6 @@ const fetchSearchResults = async (searchQuery) => {
 
 search.post("/", async (req, res) => {
   try {
-    const authHeader = req.headers.authorization;
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
-      return res.status(401).json({ success: false, message: "Unauthorized" });
-    }
-
-    const token = await verify_firebase_token(authHeader.split(" ")[1]);
-    const uid = token.uid;
-
-    const user = await Model.User.find(uid);
-    if (!user) {
-      return res.status(404).json({ success: false, message: "User doesn't exist" });
-    }
-
-    if (!user.attributes?.isRegistered) {
-      return res.status(400).json({ success: false, message: "User is not registered" });
-    }
-
     const searchQuery = req.query.query;
     if (!searchQuery || searchQuery.trim() === "") {
       return res.status(400).json({ success: false, message: "Search query is required." });
