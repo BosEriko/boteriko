5ca48be08442605bc53fd64cb828ed1ee0712d94
BosEriko <bos@eriko.ph>
2025-10-06 06:16:36 +0800

    add typing winner when setting title

--- app/controllers/Twitch/update_title/index.js
+++ app/controllers/Twitch/update_title/index.js
@@ -1,10 +1,22 @@
 const axios = require("axios");
 
+const title_generator = require("../../concerns/title_generator");
+const typing_winner = require("./typing_winner")
+
 const update_title = async (title) => {
   try {
+    const titleGenerator = title_generator(title);
+    const typingWinner = await typing_winner();
+
+    let newTitle = titleGenerator.title();
+
+    if (typingWinner) {
+      newTitle = titleGenerator.append("Previous Typing Winner", `@${typingWinner.winner}`);
+    }
+
     await axios.patch(
       `https://api.twitch.tv/helix/channels?broadcaster_id=${Config.twitch.channel.id}`,
-      { title: title },
+      { title: newTitle },
       {
         headers: {
           "Client-ID": Config.twitch.channel.clientId,
@@ -13,9 +25,9 @@ const update_title = async (title) => {
       }
     );
 
-    return `✅ Twitch title updated to "${title}"`;
+    return `✅ Twitch title updated to "${newTitle}"`;
   } catch (err) {
-    await Utility.error_logger(`❌ Failed to update title to "${title}":`, err.message);
+    await Utility.error_logger(`❌ Failed to update title to "${newTitle}":`, err.message);
   }
 }
 
