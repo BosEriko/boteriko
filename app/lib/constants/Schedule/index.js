54804664d0cf266b14e952d1a8f3398f7da278f0
BosEriko <bos@eriko.ph>
2025-10-21 22:44:12 +0800

    Feature: Update Schedule to include Wuthering Waves and Clone Hero

--- app/lib/constants/Schedule/index.js
+++ app/lib/constants/Schedule/index.js
@@ -1,9 +1,9 @@
 const Schedule = {
   Monday: { title: "Minecraft Monday 🌍⛏️", category: "Minecraft" },
   Tuesday: { title: "Tech Tuesday 💻⚙️", category: "Software and Game Development" },
-  Wednesday: { title: "Web Development Wednesday 💻⚙️", category: "Software and Game Development" },
+  Wednesday: { title: "Waifu Wednesday 💞⚔️", category: "Wuthering Waves" },
   Thursday: { title: "Tech Thursday 💻⚙️", category: "Software and Game Development" },
-  Friday: { title: "Minecraft Friday 🌍⛏️", category: "Minecraft" },
+  Friday: { title: "Frets Friday 🎸🔥", category: "Clone Hero" },
   Saturday: { title: "Side Quest Saturday 💬🎯", category: "Just Chatting" },
   Sunday: { title: "Slow Down Sunday 💻🎮", category: "Just Chatting" },
 };
