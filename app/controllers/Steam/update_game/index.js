de506b2cf0b9c341839c7302349505965265fb04
BosEriko <bos@eriko.ph>
2026-05-17 14:18:26 +0800

    Feature: Announce automatic game update to chat

--- app/controllers/Steam/update_game/index.js
+++ app/controllers/Steam/update_game/index.js
@@ -1,9 +1,10 @@
 const state = require('@global/utilities/state');
 const get_game = require('../get_game');
 
+const channelName = `#${Config.twitch.channel.username}`;
 const FIFTEEN_MINUTES = 15 * 60 * 1000;
 
-const update_game = async () => {
+const update_game = async (client) => {
   const currentGame = await get_game();
 
   if (!currentGame) return;
@@ -16,6 +17,8 @@ const update_game = async () => {
   if (!shouldUpdate) return;
 
   await Controller.Twitch.update_game(currentGame);
+
+  client.say(channelName, `🎮 Game updated to ${currentGame}`);
 
   state.timestamp.updateGame = now;
 };
