0f15a7ed5bb1731167b7e5364bf88c7af3aea764
BosEriko <bos@eriko.ph>
2026-05-18 21:11:14 +0800

    Feature: Remove character splitting on get_description

--- app/controllers/Steam/get_description/index.js
+++ app/controllers/Steam/get_description/index.js
@@ -1,4 +1,4 @@
-const get_description = async (title, charLimit = 100) => {
+const get_description = async (title) => {
   const searchRes = await fetch(`https://steamcommunity.com/actions/SearchApps/${encodeURIComponent(title)}`);
 
   const searchData = await searchRes.json();
@@ -19,13 +19,7 @@ const get_description = async (title, charLimit = 100) => {
     return null;
   }
 
-  let description = data.short_description.split(".")[0].trim();
-
-  if (description.length > charLimit) {
-    description = description.slice(0, charLimit - 3).trim() + "...";
-  }
-
-  return description;
+  return data.short_description.split(".")[0].trim();
 }
 
 module.exports = get_description;
