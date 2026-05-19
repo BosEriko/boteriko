d6f28968866f9c74d2930c342655c4394a4382e2
BosEriko <bos@eriko.ph>
2026-05-19 11:25:26 +0800

    Feature: Divide websocket into 2 files

--- app/controllers/Twitch/websocket/index.js
+++ app/controllers/Twitch/websocket/index.js
@@ -1,54 +1,11 @@
 const WebSocket = require('ws');
 const axios = require('axios');
-const { broadcastToClient } = require('@global/utilities/websocket');
+const points = require('./points');
 
 let ws;
 let reconnectUrl = null;
 let lastPing = Date.now();
 let heartbeatInterval = null;
-
-const channelName = Config.twitch.channel.username;
-
-const handleChannelPoints = async (client, payload) => {
-  const { reward, user_id, user_name, user_input, id } = payload;
-
-  try {
-    switch (reward.title) {
-      case Constant.String.BLINK:
-      case Constant.String.STRETCH:
-      case Constant.String.HYDRATE: {
-        broadcastToClient({ type: 'SOUND_ALERT', id: reward.title.toUpperCase().replace(/\s+/g, '_') });
-        client.say(channelName, `${user_name} played "${reward.title}" for ${reward.cost} channel points!`);
-        break;
-      }
-
-      case Constant.String.STAND_UP: {
-        broadcastToClient({ type: 'SOUND_ALERT', id: "STAND_UP" });
-        client.say(channelName, `${user_name} played "${reward.title}" for ${reward.cost} channel points!`);
-
-        setTimeout(() => {
-          broadcastToClient({ type: 'SOUND_ALERT', id: "SIT_DOWN" });
-          client.say(channelName, `10 minutes passed since ${user_name} redeemed "Stand Up"! Time to sit down and make your legs rest.`);
-        }, 10 * 60 * 1000);
-
-        break;
-      }
-
-      case Constant.String.ADD_TO_QUEUE: {
-        const result = await Controller.Music.add_to_queue(user_input, user_name);
-        await Controller.Twitch.update_channel_request(reward, id, result.success);
-        client.say(channelName, result.message);
-        break;
-      }
-
-      default:
-        console.warn(`⚠️ Unhandled reward: "${reward.title}"`);
-        break;
-    }
-  } catch (err) {
-    await Utility.error_logger(`Error handling reward "${reward.title}" for ${user_name}:`, err);
-  }
-};
 
 function websocket(client) {
   const accessToken = Config.twitch.channel.accessToken;
@@ -97,7 +54,7 @@ function websocket(client) {
           }
 
           case 'notification': {
-            await handleChannelPoints(client, eventPayload.event);
+            await points(client, eventPayload.event);
             break;
           }
 
---
+++ app/controllers/Twitch/websocket/points.js
@@ -0,0 +1,45 @@
+const { broadcastToClient } = require('@global/utilities/websocket');
+const channelName = Config.twitch.channel.username;
+
+async function points(client, payload) {
+  const { reward, user_id, user_name, user_input, id } = payload;
+
+  try {
+    switch (reward.title) {
+      case Constant.String.BLINK:
+      case Constant.String.STRETCH:
+      case Constant.String.HYDRATE: {
+        broadcastToClient({ type: 'SOUND_ALERT', id: reward.title.toUpperCase().replace(/\s+/g, '_') });
+        client.say(channelName, `${user_name} played "${reward.title}" for ${reward.cost} channel points!`);
+        break;
+      }
+
+      case Constant.String.STAND_UP: {
+        broadcastToClient({ type: 'SOUND_ALERT', id: "STAND_UP" });
+        client.say(channelName, `${user_name} played "${reward.title}" for ${reward.cost} channel points!`);
+
+        setTimeout(() => {
+          broadcastToClient({ type: 'SOUND_ALERT', id: "SIT_DOWN" });
+          client.say(channelName, `10 minutes passed since ${user_name} redeemed "Stand Up"! Time to sit down and make your legs rest.`);
+        }, 10 * 60 * 1000);
+
+        break;
+      }
+
+      case Constant.String.ADD_TO_QUEUE: {
+        const result = await Controller.Music.add_to_queue(user_input, user_name);
+        await Controller.Twitch.update_channel_request(reward, id, result.success);
+        client.say(channelName, result.message);
+        break;
+      }
+
+      default:
+        console.warn(`⚠️ Unhandled reward: "${reward.title}"`);
+        break;
+    }
+  } catch (err) {
+    await Utility.error_logger(`Error handling reward "${reward.title}" for ${user_name}:`, err);
+  }
+};
+
+module.exports = points;
