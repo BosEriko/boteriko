193e8150a07f3affa9812f0ce4f4c64de135d0d7
BosEriko <bos@eriko.ph>
2026-05-23 12:11:57 +0800

    Feature: Update logic of update_twitch so that it updates achievement every minute

--- app/controllers/Steam/update_twitch/index.js
+++ app/controllers/Steam/update_twitch/index.js
@@ -7,6 +7,8 @@ const get_id = require('../get_id');
 const channelName = `#${Config.twitch.channel.username}`;
 
 const update_twitch = async (client) => {
+  let currentId;
+
   const currentGame = await get_game();
   if (!currentGame) {
     state.steam.gameName = null;
@@ -15,19 +17,28 @@ const update_twitch = async (client) => {
     state.steam.gamePercent = null;
     return;
   }
-  if (state.steam.gameName === currentGame) return;
-  client.say(channelName, await Controller.Twitch.update_game(currentGame));
-  state.steam.gameName = currentGame;
 
-  const currentId = await get_id(currentGame);
-  if (!currentId) return;
-  state.steam.gameId = currentId;
-  state.steam.gamePercent = await get_achievement(currentId);
+  if (state.steam.gameName !== currentGame) {
+    client.say(channelName, await Controller.Twitch.update_game(currentGame));
+    state.steam.gameName = currentGame;
 
-  const currentDescription = await get_description(currentId);
-  if (!currentDescription) return;
-  client.say(channelName, await Controller.Twitch.update_title(currentDescription));
-  state.steam.gameDescription = currentDescription;
+    currentId = await get_id(currentGame);
+    if (currentId) {
+      state.steam.gameId = currentId;
+      const currentDescription = await get_description(currentId);
+      if (currentDescription) {
+        client.say(channelName, await Controller.Twitch.update_title(currentDescription));
+        state.steam.gameDescription = currentDescription;
+      } else {
+        client.say(channelName, await Controller.Twitch.update_title(currentGame))
+        state.steam.gameDescription = null;
+      };
+    };
+  };
+
+  if (currentId) {
+    state.steam.gamePercent = await get_achievement(currentId);
+  }
 };
 
 module.exports = update_twitch;
