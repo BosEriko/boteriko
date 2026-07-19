a9933f560376d605f55bc22a78e876073c25fff4
BosEriko <bos@eriko.ph>
2026-07-19 16:35:18 +0800

    Feature: Remove extra space

--- bin/start.js
+++ bin/start.js
@@ -2,6 +2,5 @@ require('module-alias/register');
 require('../config/initializers/preload');
 require('../app/services/bot/discord');
 require('../app/services/bot/twitch');
-
 const { bootstrap } = require('../src/main');
 bootstrap();
