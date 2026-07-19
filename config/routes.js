0b194c28ac4f2233862620a004454d47d881ba04
BosEriko <bos@eriko.ph>
2026-07-19 15:30:11 +0800

    ♻️ Change /api routes to /legacy

--- config/routes.js
+++ config/routes.js
@@ -21,42 +21,42 @@ app.use('/public', express.static(path.join(__dirname, '..', 'public')));
 app.get('/', (req, res) => { res.redirect(Config.app.clientUrl) });
 
 // Authentication Routes
-app.use('/api/authentication/discord/callback', Controller.Discord.authentication_callback);
-app.use('/api/authentication/discord/connect', Controller.Discord.authentication_connect);
-app.use('/api/authentication/spotify/callback', Controller.Music.authentication_callback);
-app.use('/api/authentication/spotify/login', Controller.Music.authentication_login);
-app.use('/api/authentication/tetrio/connect', Controller.Tetrio.authentication_connect);
-app.use('/api/authentication/twitch/callback', Controller.Twitch.authentication_callback);
-app.use('/api/authentication/twitch/login', Controller.Twitch.authentication_login);
+app.use('/legacy/authentication/discord/callback', Controller.Discord.authentication_callback);
+app.use('/legacy/authentication/discord/connect', Controller.Discord.authentication_connect);
+app.use('/legacy/authentication/spotify/callback', Controller.Music.authentication_callback);
+app.use('/legacy/authentication/spotify/login', Controller.Music.authentication_login);
+app.use('/legacy/authentication/tetrio/connect', Controller.Tetrio.authentication_connect);
+app.use('/legacy/authentication/twitch/callback', Controller.Twitch.authentication_callback);
+app.use('/legacy/authentication/twitch/login', Controller.Twitch.authentication_login);
 
 // Detail Routes
-app.use('/api/detail/tetrio', Controller.Detail.tetrio);
-app.use('/api/detail/twitch', Controller.Detail.twitch);
+app.use('/legacy/detail/tetrio', Controller.Detail.tetrio);
+app.use('/legacy/detail/twitch', Controller.Detail.twitch);
 
 // User Routes
-app.use('/api/user/deactivate', Controller.User.deactivate);
-app.use('/api/user/profile', Controller.User.profile);
+app.use('/legacy/user/deactivate', Controller.User.deactivate);
+app.use('/legacy/user/profile', Controller.User.profile);
 
 // Game Routes
-app.use('/api/game/profile', Controller.Game.profile);
-app.use('/api/game/update_list', Controller.Game.update_list);
+app.use('/legacy/game/profile', Controller.Game.profile);
+app.use('/legacy/game/update_list', Controller.Game.update_list);
 
 // Anime Routes
-app.use('/api/anime/profile', Controller.Anime.profile);
+app.use('/legacy/anime/profile', Controller.Anime.profile);
 
 // Manga Routes
-app.use('/api/manga/profile', Controller.Manga.profile);
+app.use('/legacy/manga/profile', Controller.Manga.profile);
 
 // Data Routes
-app.use('/api/data/initial', Controller.Data.initial);
-app.use('/api/data/search', Controller.Data.search);
-app.use('/api/data/overlay', Controller.Data.overlay);
+app.use('/legacy/data/initial', Controller.Data.initial);
+app.use('/legacy/data/search', Controller.Data.search);
+app.use('/legacy/data/overlay', Controller.Data.overlay);
 
 // TETR.IO Routes
-app.use('/api/tetrio/disconnect', Controller.Tetrio.disconnect);
+app.use('/legacy/tetrio/disconnect', Controller.Tetrio.disconnect);
 
 // Discord Routes
-app.use('/api/discord/disconnect', Controller.Discord.disconnect);
+app.use('/legacy/discord/disconnect', Controller.Discord.disconnect);
 
 const server = http.createServer(app);
 
